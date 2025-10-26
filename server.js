558c4dc9294cbc48b8af1ae6954fd8d7ecdcdd92
Muhammed Nabeel kk <103543585+nabeelkk@users.noreply.github.com>
2025-10-27 04:08:06 +0530

    Update CORS origin for socket server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 const server = http.createServer(app)
 
 export const io = new Server(server,{
-    cors : {origin: "http://localhost:5173"}
+    cors : {origin: "https://pinet-real-time-chating-app-client.vercel.app/"}
 })
 
 export const userSocketMap = {} // {userId: socketId}
